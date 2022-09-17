bfe79675337ba1535a26b39aba6d90d79ebc702f
pavel <dubovikdmc@gmail.com>
2022-09-17 22:41:36 +0300

    verstka

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,27 @@
 import React from 'react';
 import './App.css';
 
-import Header from "./Header";
-import Technologies from "./Technologies";
 
 const App = () => {
     return (
-        <div>
-            <Header />
-            <Technologies />
+        <div className={"app-wrapper"}>
+            <header className={"header"}>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/SVG_Logo.svg/512px-SVG_Logo.svg.png"
+                     alt=""/>
+            </header>
+            <nav className={"nav"}>
+                <ul>
+                    <li><a href="#">Profile</a></li>
+                    <li><a href="#">Messages</a></li>
+                    <li><a href="#">News</a></li>
+                    <li><a href="#">Music</a></li>
+                    <li><a href="#">Settings</a></li>
+                </ul>
+            </nav>
+            <div className={"content"}><img src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg" alt=""/></div>
         </div>
     );
 }
-
-
-
 
 
 export default App;
