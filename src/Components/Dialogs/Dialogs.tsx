48ab6cb486b617ee97e6e9dc9d126c4d8c1fd756
pavel <dubovikdmc@gmail.com>
2022-10-10 19:42:24 +0300

    21 lesson

--- src/Components/Dialogs/Dialogs.tsx
+++ src/Components/Dialogs/Dialogs.tsx
@@ -3,8 +3,32 @@ import s from "./Dialogs.module.css"
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.users}>
+                <div className={s.user + " " + s.active}>
+                    Dima
+                </div>
+                <div className={s.user}>
+                    Pasha
+                </div>
+                <div className={s.user}>
+                    Sasha
+                </div>
+                <div className={s.user}>
+                    Liza
+                </div>
+                <div className={s.user}>
+                    Yana
+                </div>
+                <div className={s.user}>
+                    Danik
+                </div>
+            </div>
+            <div className={s.messeges}>
+                <div className={s.messege}>Hello</div>
+                <div className={s.messege}>How are you?</div>
+                <div className={s.messege}>Mean</div>
+            </div>
         </div>
     );
 };
